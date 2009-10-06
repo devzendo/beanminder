83032be74603a901a5d8e752fdf098fa8232c0e3
matt <unknown>
2009-10-06 20:25:02 +0000

    Extracted the plugin helper from the PersistencePluginHelper (which'll be renamed next). Provided another implementation of plugin loading which is now used in the PluginManager - now does not need mycila, and this'll be the start of loading plugins written in JRuby. Removed the deprecated update site URL retrieval method from PluginManager.

--- src/test/java/uk/me/gumbley/simpleaccounts/plugin/TestPluginLoading.java
+++ src/test/java/uk/me/gumbley/simpleaccounts/plugin/TestPluginLoading.java
@@ -3,8 +3,8 @@ package uk.me.gumbley.simpleaccounts.plugin;
 import org.junit.Assert;
 import org.junit.Test;
 
-import uk.me.gumbley.minimiser.persistence.PersistencePluginHelper;
 import uk.me.gumbley.minimiser.pluginmanager.PluginException;
+import uk.me.gumbley.minimiser.pluginmanager.PluginHelper;
 
 /**
  * Tests that this project's plugin is loaded.
@@ -15,18 +15,19 @@ import uk.me.gumbley.minimiser.pluginmanager.PluginException;
 public final class TestPluginLoading {
     /**
      * Load standard plugins, test ours is the application.
-     * @throws PluginException on failure
+     * 
+     * @throws PluginException
+     *         on failure
      */
     @Test
     public void ourPluginIsLoaded() throws PluginException {
         try {
-        final PersistencePluginHelper persistencePluginHelper = new PersistencePluginHelper();
-        persistencePluginHelper.loadStandardPlugins();
-        Assert.assertTrue(
-            "Our plugin is not loaded",
-            persistencePluginHelper.getApplicationPlugin()
-            instanceof SimpleAccountsApplicationPlugin);
-        } catch (Throwable t) {
+            final PluginHelper pluginHelper = new PluginHelper(false);
+            pluginHelper.loadStandardPlugins();
+            Assert.assertTrue(
+                "Our plugin is not loaded",
+                pluginHelper.getApplicationPlugin() instanceof SimpleAccountsApplicationPlugin);
+        } catch (final Throwable t) {
             t.printStackTrace();
         }
     }
